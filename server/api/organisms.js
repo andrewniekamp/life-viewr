5b20b2cc7e5bdeef7e90b30877339acac05911a1
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-12-12 15:50:09 -0500

    Add api route to add organism

--- server/api/organisms.js
+++ server/api/organisms.js
@@ -7,3 +7,21 @@ router.get('/', (req, res, next) => {
     .then(organisms => res.json(organisms))
     .catch(next)
 })
+
+router.post('/', (req, res, next) => {
+  // Likely preferable to verify data before creating
+  let data = req.body;
+  Organism.create({
+    name: data.name,
+    imageURL: data.imageURL,
+    kingdom: { name: data.kingdom.name },
+    phylum: { name: data.phylum.name },
+    class: { name: data.class.name },
+    order: { name: data.order.name },
+    family: { name: data.family.name },
+    genus: { name: data.genus.name },
+    species: { name: data.species.name }
+  }, {include: [{ all: true }]})
+    .then(organism => res.json(organism))
+    .catch(next)
+})
